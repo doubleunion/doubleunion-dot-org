bad5196abd7185bcd01ea2b03e6afc2b2f52f18a
Vivian <vivian.brown@gmail.com>
2015-04-22 08:48:20 -0700

    Change Leadership page to section on About page. Add Tina's bio photo. Remove Lillie and Leigh for now.

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   get 'supporters',       to: 'static_pages#supporters'
   get 'visit',            to: 'static_pages#visit'
   get 'base_assumptions', to: 'static_pages#base_assumptions'
-  get 'board', to: 'static_pages#leadership'
+  get 'about',            to: 'static_pages#about'
 
   # For all those browser autocompletes still pointing at the www domain
   get '*path' => redirect(DU_APP_URL) if Rails.env.production?
