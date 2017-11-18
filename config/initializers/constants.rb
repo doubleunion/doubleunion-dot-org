400ac221f84e2594e348d028e30d5a8473324ee4
Chandrika Achar <chandrika.achar@gmail.com>
2017-11-18 15:05:03 -0800

    Update mailing list link

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -11,6 +11,6 @@ MEMBERSHIP_EMAIL     = I18n.t('du.membership_email')
 TUMBLR_BASE          = 'doubleunion.tumblr.com'
 TUMBLR_URL           = "http://#{TUMBLR_BASE}"
 
-MAILING_LIST_GENERAL = "http://lists.doubleunion.org/listinfo.cgi/doubleunion-doubleunion.org"
+MAILING_LIST_GENERAL = "https://groups.google.com/a/doubleunion.org/forum/#!forum/public"
 GOOGLE_ANALYTICS_ID  = 'UA-47411942-1'
 S3_BUCKET            = "https://s3-us-west-1.amazonaws.com/doubleunion"
